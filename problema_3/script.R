6fb85e3a75decd7e6aeb13d0a0fe31b6b74d87f4
jmigual <j.marce.igual@gmail.com>
2016-11-12 16:26:47 +0100

    Begin implementing SVD solution

--- problema_3/script.R
+++ problema_3/script.R
@@ -1,7 +1,18 @@
 # Introduim les dades
 X = matrix(c(rep(1,5),seq(5)),ncol=2)
 t = c(7.97, 10.2, 14.2, 16.0, 21.2)
+print(X)
+print(t)
 
 # Calculem la matriu pseudo-inversa
 pseudo = solve(t(X) %*% X) %*% t(X)
 w.pseudo = pseudo %*% t
+print(pseudo)
+print(w.pseudo)
+
+# Calcular svd de la matriu de disseny
+svd = svd(X)
+print(svd)
+
+D = diag(2)*(1/svd$d)
+
